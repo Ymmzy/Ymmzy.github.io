6e760334cf1857d5ae5dc14ce9bb058b4940ed4c
Ymmzy <33130252+Ymmzy@users.noreply.github.com>
2025-01-27 18:54:40 +0800

    Delete gooboo-1.5.8 directory

--- gooboo-1.5.8/cypress/e2e/note.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Notes load properly', () => {
-    it('notes can be viewed', () => {
-        cy.toFeature('Debug');
-        cy.contains('1m').click();
-
-        cy.contains('Read').click();
-        cy.contains('Use pickaxe');
-    });
-});
--- gooboo-1.5.8/src/js/modules/migration/v1_4_1.js
+++
@@ -1,7 +0,0 @@
-export default function(save) {
-    // Update offering stats
-    if (save.stat.village_offering) {
-        save.stat.village_offeringAmount = save.stat.village_offering;
-    }
-    return save;
-}
